f7a7fb63eae711b79147b898eb2db5f73d14e778
koushikbalusu <koushikchowdhary11469@gmail.com>
2025-11-11 15:41:34 +0530

    empty

--- src/components/providers/client-layout.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { ReactNode } from "react";
-import { AuthProvider } from "./auth-provider";
-
-export default function ClientLayout({ children }: { children: ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}

4c123783effdd9eaf81c99172093ffe480dc0aca
koushikbalusu <koushikchowdhary11469@gmail.com>
2025-12-27 12:24:26 +0530

    jgvgubhjg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ import { m } from "motion/react";
 export default function HomePage() {
   return (
     <div className="flex min-h-screen flex-col bg-background" style={{ margin: 0, padding: 0 }}>
-      {/* <SiteHeader />
+      <SiteHeader />
       <main className="flex-1">
         <VideoHero viewCarsHref="/cars" />
         <OfferBannerSection />
       </main>
-      <SiteFooter /> */}
-      <h1 style={{top: "50%", left: "30%", fontSize: "100px", position: "absolute", transformOrigin: "center"}}>Text me +917989838534</h1>
+      <SiteFooter />
+      {/* <h1 style={{top: "50%", left: "30%", fontSize: "100px", position: "absolute", transformOrigin: "center"}}>Text me +917989838534</h1> */}
     </div>
   );
 }
